6ffd71a5763fc264d101669427937536bf710bdc
michael <michael@substance.io>
2014-10-21 22:11:13 +0200

    Register urls in article metadata.

--- data/article_meta_data.js
+++ data/article_meta_data.js
@@ -2,41 +2,49 @@ module.exports = {
   "http://dx.doi.org/10.7554/eLife.00461": {
     title: "Ribosome structures to near-atomic resolution from thirty thousand cryo-EM particles",
     authors: ["Xiao-chen Bai", "Israel S Fernandez", "Greg McMullan", "Sjors HW Scheres"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/00461/elife00461.xml#related_articles/all",
     published_on: "2013-02-19"
   },
   "http://dx.doi.org/10.7554/eLife.03665": {
     title: "Beam-induced motion correction for sub-megadalton cryo-EM particles",
     authors: ["Sjors HW Scheres"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/03665/elife03665.xml#related_articles/all",
     published_on: "2014-08-13"
   },
   "http://dx.doi.org/10.7554/eLife.03678": {
     title: "Microscopy: Cryo-EM enters a new era",
     authors: ["Werner Kühlbrandt"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/03678/elife03678.xml#related_articles/all",
     published_on: "2014-08-13"
   },
   "http://dx.doi.org/10.7554/eLife.02763": {
     title: "Biosensors: Plant biologists FRET over stress",
     authors: ["Won-Gyu Choi", "Simon Gilroy"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/02763/elife02763.xml#related_articles/all",
     published_on: "2014-04-15"
   },
   "http://dx.doi.org/10.7554/eLife.01739": {
     title: "FRET-based reporters for the direct visualization of abscisic acid concentration changes and distribution in Arabidopsis",
     authors: ["Rainer Waadt", "Kenichi Hitomi", "Noriyuki Nishimura", "Chiharu Hitomi", "Stephen R Adams", "Elizabeth D Getzoff", "Julian I Schroeder"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/01739/elife01739.xml#related_articles/all",
     published_on: "2014-04-15"
   },
   "http://dx.doi.org/10.7554/eLife.01741": {
     title: "Abscisic acid dynamics in roots detected with genetically encoded FRET sensors",
     authors: ["Alexander M Jones", "Jonas ÅH Danielson", "Shruti N ManojKumar", "Viviane Lanquar", "Guido Grossmann", "Wolf B Frommer"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/01741/elife01741.xml#related_articles/all",
     published_on: "2014-04-15"
   },
   "http://dx.doi.org/10.1371/journal.pbio.0030356": {
     title: "Modeling T Cell Antigen Discrimination Based on Feedback Control of Digital ERK Responses",
     authors: ["Grégoire Altan-Bonnet", "Ronald N Germain"],
+    url: "http://dx.doi.org/10.1371/journal.pbio.0030356",
     published_on: "2005-10-25"
   },
   "http://dx.doi.org/10.7554/eLife.00778": {
     title: "Direct single molecule measurement of TCR triggering by agonist pMHC in living primary T cells",
     authors: ["Geoff P O'Donoghue", "Rafal M Pielak", "Alexander A Smoligovets", "Jenny J Lin", "Jay T Groves"],
+    url: "/doc.html?url=https://s3.amazonaws.com/elife-cdn/elife-articles/00778/elife00778.xml#related_articles/all",
     published_on: "2014-08-13"
   }
 };
