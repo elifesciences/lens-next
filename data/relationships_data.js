2b317012799ad17db402b90ed685c52006100c79
michael <michael@substance.io>
2014-10-21 22:11:35 +0200

    Use new relationship data model.

--- data/relationships_data.js
+++ data/relationships_data.js
@@ -1,36 +1,69 @@
 module.exports = [
+  // source is key-reference of target (experiment)
   {
-    source: "http://dx.doi.org/10.7554/eLife.00778",
-    target: "http://dx.doi.org/10.1016/j.immuni.2011.09.013",
-    type: 'key-reference',
-    description: 'This <b>blaaaa</b> is choosen as a <i>key-reference</i> because it is so awesome.',
-    creator: ["Mickey Mouse"]
+    id: "key_reference_348989",
+    source: "http://dx.doi.org/10.1016/j.immuni.2011.09.013",
+    target: "http://dx.doi.org/10.7554/eLife.00778",
+    type: 'key_reference',
+    key_reference_type: "experiment",
+    description: 'A ground breaking experiment can be found here.',
+    creators: [],
+    created_at: "2014-10-21T11:04:11.547Z"
   },
+  // source is key-reference of target (experiment)
   {
-    source: "http://dx.doi.org/10.7554/eLife.00778",
+    id: "key_reference_348989",
+    source: "http://dx.doi.org/10.1016/j.immuni.2011.09.013",
     target: "http://dx.doi.org/10.1371/journal.pbio.0030356",
-    type: 'key-reference',
-    description: 'This blaaaa is choosen as a key-reference because it is so awesome.',
-    creator: ["Mickey Mouse"]
+    type: 'key_reference',
+    key_reference_type: "key-data",
+    description: 'Key data can be found here.',
+    creators: [],
+    created_at: "2014-10-21T11:04:11.547Z"
   },
+  // source is advance of target
   {
     source: "http://dx.doi.org/10.7554/eLife.03665",
     target: "http://dx.doi.org/10.7554/eLife.00461",
     type: 'advance',
-    description: 'This advances the proposed method by making it better.',
-    creator: ["Donald Duck"]
+    description: 'Building on previous work (Bai et al., 2013), we describe an algorithm that allows cryo-EM structure determination to near-atomic resolution for protein complexes as small as 170 kDa.',
+    creators: [{name: "Donald Duck", orcid: "0000-2342-5674-0043"}],
+    created_at: "2014-10-21T11:04:11.547Z"
   },
+  // source is insight of target
   {
     source: "http://dx.doi.org/10.7554/eLife.03678",
     target: "http://dx.doi.org/10.7554/eLife.03665",
     type: 'insight',
-    description: 'This provides an insight into the proposed work.',
-    creator: ["Goofy"]
+    description: 'Advances in detector hardware and image-processing software have led to a revolution in the use of electron cryo-microscopy to determine complex molecular structures at high resolution.',
+    creators: [{name: "Werner Kühlbrandt", orcid: "0000-2342-5674-0099"}],
+    created_at: "2014-10-21T11:04:11.547Z"
   },
+  // source is co-publication of target and vice versa (#1)
   {
-    target: ["http://dx.doi.org/10.7554/eLife.02763", "http://dx.doi.org/10.7554/eLife.01739", "http://dx.doi.org/10.7554/eLife.01741"],
-    type: 'co-published',
+    source: "http://dx.doi.org/10.7554/eLife.01739", 
+    target: "http://dx.doi.org/10.7554/eLife.02763",
+    type: 'co_publication',
     description: 'This article has been co-published.',
-    creator: ["Dagobert Duck"]
+    creators: [{name: "Daisy Duck", orcid: "0000-2342-5674-0055"}],
+    created_at: "2014-10-21T11:04:11.547Z"
+  },
+  // source is co-publication of target and vice versa (#2)
+  {
+    source: "http://dx.doi.org/10.7554/eLife.01739", 
+    target: "http://dx.doi.org/10.7554/eLife.01741",
+    type: 'co_publication',
+    description: 'This article has been co-published.',
+    creators: [{name: "Daisy Duck", orcid: "0000-2342-5674-0055"}],
+    created_at: "2014-10-21T11:04:11.547Z"
+  },
+  // source is co-publication of target and vice versa (#3)
+  {
+    source: "http://dx.doi.org/10.7554/eLife.01741", 
+    target: "http://dx.doi.org/10.7554/eLife.02763",
+    type: 'co_publication',
+    description: 'This article has been co-published.',
+    creators: [{name: "Daisy Duck", orcid: "0000-2342-5674-0055"}],
+    created_at: "2014-10-21T11:04:11.547Z"
   }
 ];
