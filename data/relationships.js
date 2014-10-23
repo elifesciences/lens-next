33a273a65f6327a0473f463efc3a8fe90114e52c
michael <michael@substance.io>
2014-10-23 23:16:42 +0200

    Data model improvements.
    - Use simple creator property
    - Introduce relationship_type_name to determine the label in the view
    (maybe rename relationship_label?)

--- data/relationships.js
+++ data/relationships.js
@@ -4,10 +4,10 @@ module.exports = {
   // Research Advance Usecase
   "http://dx.doi.org/10.7554/eLife.00461": [
     {
-      "id": "relationship_1",
       "relationship_type": 'advance',
+      "relationship_type_name": 'Research Advance',
       "description": 'Building on previous work (Bai et al., 2013), we describe an algorithm that allows cryo-EM structure determination to near-atomic resolution for protein complexes as small as 170 kDa.',
-      "creators": [{"name": "Donald Duck", "orcid": "0000-2342-5674-0043"}],
+      "creator": "Donald Duck",
       "created_at": "2014-10-21T11:04:11.547Z",
       "meta": {
 
@@ -25,10 +25,10 @@ module.exports = {
   // Insight Usecase
   "http://dx.doi.org/10.7554/eLife.03665": [
     {
-      "id": "relationship_2",
       "relationship_type": 'insight',
+      "relationship_type_name": 'Insight',
       "description": 'Advances in detector hardware and image-processing software have led to a revolution in the use of electron cryo-microscopy to determine complex molecular structures at high resolution.',
-      "creators": [{"name": "Werner Kühlbrandt", "orcid": "0000-2342-5674-0099"}],
+      "creator": "Werner Kühlbrandt",
       "created_at": "2014-10-21T11:04:11.547Z",
       "meta": {
 
@@ -46,10 +46,10 @@ module.exports = {
   // Co-published Usecase
   "http://dx.doi.org/10.7554/eLife.02763": [
     {
-      "id": "relationship_3",
       "relationship_type": 'co-published',
+      "relationship_type_name": 'Co-Published',
       "description": 'This article has been co-published.',
-      "creators": [{"name": "Daisy Duck", "orcid": "0000-2342-5674-0055"}],
+      "creator": "Daisy Duck",
       "created_at": "2014-10-21T11:04:11.547Z",
       "meta": {
 
@@ -63,10 +63,10 @@ module.exports = {
       }
     },
     {
-      "id": "relationship_4",
       "relationship_type": 'co-published',
+      "relationship_type_name": 'Co-Published',
       "description": 'This article has been co-published.',
-      "creators": [{"name": "Daisy Duck", "orcid": "0000-2342-5674-0055"}],
+      "creator": "Daisy Duck",
       "created_at": "2014-10-21T11:04:11.547Z",
       "meta": {
 
@@ -84,10 +84,10 @@ module.exports = {
   // Registered Report Usecase 2 - registered report pointing to original article and replication study  
   "http://dx.doi.org/10.7554/eLife.03696": [
     {
-      "id": "relationship_1",
-      "relationship_type": 'original work',
+      "relationship_type": 'original-work',
+      "relationship_type_name": "Original Work",
       "description": 'This paper was selected as part of the Cancer Biology Replication study, for more information see <a href="reproducability.org">the reproducablity project</a>',
-      "creators": [{"name": "Donald Duck", "orcid": "0000-2342-5674-0043"}],
+      "creator": "Donald Duck",
       "created_at": "2014-22-21T11:04:11.547Z",
       "meta": {
       },
@@ -100,13 +100,13 @@ module.exports = {
       }
     },
     {
-      "id": "relationship_2",
-      "relationship_type": 'replication study',
+      "relationship_type": 'replication-study',
+      "relationship_type_name": "Replication Study",
       "description": 'This is the full replication report for the original work listed as part of the <a href="reproducability.org">the reproducablity project</a>',
-      "creators": [{"name": "Donald Duck", "orcid": "0000-2342-5674-0043"}],
+      "creator": "Donald Duck",
       "created_at": "2014-10-21T11:04:11.547Z",
       "meta": {
-     
+        
       },
       "related_article": {
         "doi": "http://dx.doi.org/10.7554/eLife.03251",
@@ -121,10 +121,10 @@ module.exports = {
   // Registered Report Usecase 3 - replication study  pointing to original article and registered report
   "http://dx.doi.org/10.7554/eLife.03251": [
     {
-      "id": "relationship_1",
-      "relationship_type": 'original work',
+      "relationship_type": 'original-work',
+      "relationship_type_name": 'Original Work',
       "description": 'This paper was selected as part of the Cancer Biology Replication study, for more information see <a href="reproducability.org">the reproducablity project</a>.',
-      "creators": [{"name": "Donald Duck", "orcid": "0000-2342-5674-0043"}],
+      "creator": "Donald Duck",
       "created_at": "2014-12-23T11:04:11.547Z",
       "meta": {
    
@@ -138,10 +138,10 @@ module.exports = {
       }
     },
     {
-      "id": "relationship_2",
-      "relationship_type": 'registered report',
+      "relationship_type": 'registered-report',
+      "relationship_type_name": 'Registered Report',
       "description": 'This registered report outlines the approach agreed prior to undertaking this replications study as part of <a href="reproducability.org">the reproducablity project</a>.',
-      "creators": [{"name": "Donald Duck", "orcid": "0000-2342-5674-0043"}],
+      "creator": "Donald Duck",
       "created_at": "2014-12-23T11:04:11.547Z",
       "meta": {
      
